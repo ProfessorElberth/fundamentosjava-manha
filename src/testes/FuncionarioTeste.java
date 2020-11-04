2e9933754ed6775edc9b1d502d1ad06c33b426cc
elberth moraes <elberth.moraes@gmail.com>
2020-11-04 09:14:02 -0300

    Criacao da classe de funcionario, de teste de funcionario e de constantes. Atualizacao da classe Segundo

--- src/testes/FuncionarioTeste.java
+++ src/testes/FuncionarioTeste.java
@@ -5,27 +5,18 @@ public class FuncionarioTeste {
 	public static void main(String[] args) {
 		
 		Funcionario func1 = new Funcionario();
-		func1.nome = "elberth";
 		func1.idade = 42;
 		func1.salario = 1000;
 		func1.bonus = 500;
 		func1.desconto = 200;
-		System.out.println("Funcionário: " + func1.nome);
+		func1.exibir();
 
-		Funcionario func2 = new Funcionario();
-		func2.nome = "lucas";
-		func2.idade = 22;
-		func2.salario = 10000;
-		func2.bonus = 5000;
-		func2.desconto = 2000;
-		System.out.println("Funcionário: " + func2.idade);
+		new Funcionario("lucas", 22, 10000, 5000, 2000).exibir();
 
-		Funcionario func3 = new Funcionario();
-		func3.nome = "carla";
-		func3.idade = 21;
+		Funcionario func3 = new Funcionario("carla", 21);
 		func3.salario = 20000;
 		func3.bonus = 6000;
 		func3.desconto = 3000;
-		System.out.println("Funcionário: " + func3);
+		func3.exibir();
 	}
 }
